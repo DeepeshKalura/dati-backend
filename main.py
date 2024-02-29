bd654edb22790bdd538009234c81fc1444f44a8a
DeepeshKalura <deepeshkalurs@gmail.com>
2024-02-29 21:06:58 +0530

    Add static file serving and update root endpoint

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from tempfile import NamedTemporaryFile
 from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 import json
 
 from app.logic import convert_pdf_to_images, extract_text_from_img, extract_structured_data
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="template"), name="static")
 
-@app.get("/")
+@app.get("/",  response_class=FileResponse)
 async def read_root():
-    return {"Hello": "World"}
+    return FileResponse('template/index.html')
 
 
 @app.post("/uploadfile/")
@@ -26,17 +29,16 @@ async def create_upload_file(file: UploadFile = File(...)):
     path = f'{file.filename}'
 
     results = []
-    with NamedTemporaryFile(dir='.', suffix='.csv') as f:
 
-        pdf_image_list = convert_pdf_to_images(path)
-        pdf_text_list = extract_text_from_img(pdf_image_list)
-        
-        data = extract_structured_data(pdf_text_list, default_data_points)
-        json_data = json.loads(data)
-        if isinstance(json_data, list):
-            results.extend(json_data)  # Use extend() for lists
-        else:
-            results.append(json_data)
+    pdf_image_list = convert_pdf_to_images(path)
+    pdf_text_list = extract_text_from_img(pdf_image_list)
+    
+    data = extract_structured_data(pdf_text_list, default_data_points)
+    json_data = json.loads(data)
+    if isinstance(json_data, list):
+        results.extend(json_data)  # Use extend() for lists
+    else:
+        results.append(json_data)
         
     
     return results

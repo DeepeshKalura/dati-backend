890ef8610bbd1390a646fd4c12c84882c162d30a
DeepeshKalura <deepeshkalurs@gmail.com>
2024-03-04 01:37:14 +0530

    Update .gitignore and add OCR and multimodel endpoints

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from fastapi.staticfiles import StaticFiles
 from typing import Optional, Dict
 import json
 
-from app.logic import convert_pdf_to_images, extract_text_from_img, extract_structured_data
+from app.ocr import convert_pdf_to_images, extract_text_from_img, extract_structured_data
+
+import app.multimodel as mm
 
 
 app = FastAPI()
@@ -16,19 +18,15 @@ async def read_root():
     return FileResponse('template/index.html')
 
 
-@app.post("/autmoate/")
-async def create_upload_file(file: UploadFile = File(...), data: Optional[str] = None):
+@app.post("/autmoate/ocr/")
+async def ocr_upload_file(file: UploadFile = File(...), data: Optional[str] = None):
     if (data == None):
-        data_points = """{
+        data = """{
             "invoice_item": "what is the item that charged",
             "Amount": "how much does the invoice item cost in total",
             "Company_name": "company that issued the invoice",
             "invoice_date": "when was the invoice issued",
         }"""
-
-    else :
-        data_points = data
-        print(data_points)
     
     with open(file.filename, "wb") as buffer:
         buffer.write(file.file.read())
@@ -39,7 +37,7 @@ async def create_upload_file(file: UploadFile = File(...), data: Optional[str] =
     pdf_image_list = convert_pdf_to_images(path)
     pdf_text_list = extract_text_from_img(pdf_image_list)
     
-    extract_data = extract_structured_data(pdf_text_list, data_points)
+    extract_data = extract_structured_data(pdf_text_list, data)
     json_data = json.loads(extract_data)
     if isinstance(json_data, list):
         results.extend(json_data) 
@@ -48,3 +46,33 @@ async def create_upload_file(file: UploadFile = File(...), data: Optional[str] =
         
     
     return results
+
+
+@app.post("/autmoate/multimodel/")
+async def multimodel_upload_file(file: UploadFile = File(...), data: Optional[str] = None):
+    if(data == None):
+        data = """{
+            "invoice_item": "what is the item that charged",
+            "Amount": "how much does the invoice item cost in total",
+            "Company_name": "company that issued the invoice",
+            "invoice_date": "when was the invoice issued",
+        }"""
+
+    with open(file.filename, "wb") as buffer:
+        buffer.write(file.file.read())
+    path = f'{file.filename}'
+
+    results = []
+
+    mm.convert_pdf_to_images(file_path=path)
+    extract_data = mm.extract_structured_data(data)
+    json_data = json.loads(extract_data)
+    if isinstance(json_data, list):
+        results.extend(json_data) 
+    else:
+        results.append(json_data)
+
+    return results
+
+
+    
